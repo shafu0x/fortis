1bac3c9f16eae4db88cd59dac859bd94e2c6de6a
shafu <shafu0x@gmail.com>
2024-12-19 20:18:51 +0100

    manager

--- contracts/src/Manager.sol
+++ contracts/src/Manager.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.26;
 
-contract Manager {
+import {FUSD} from "./FUSD.sol";
 
+contract Manager {
+    FUSD public fusd;
+
+    constructor() {
+        fusd = new FUSD();
+    }
 }
