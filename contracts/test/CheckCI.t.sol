329990ef917f65a3c6279cdd67c3e26586ca07a9
shafu <shafu0x@gmail.com>
2024-12-19 19:47:33 +0100

    add ci

---
+++ contracts/test/CheckCI.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.26;
+
+import {Test} from "forge-std/src/Test.sol";
+
+contract CI_Test is Test {
+    function test_pass() external pure {
+        assertEq(true, true);
+    }
+}
