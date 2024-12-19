cc3a1adbd5aa1f2d7ca5970106c1f9ec5ae85f03
shafu <shafu0x@gmail.com>
2024-12-19 19:48:43 +0100

    refactor

--- contracts/test/CheckCI.t.sol
+++ contracts/test/CheckCI.t.sol
@@ -4,7 +4,7 @@ pragma solidity =0.8.26;
 import {Test} from "forge-std/src/Test.sol";
 
 contract CI_Test is Test {
-    function test_pass() external pure {
+    function test_ci_pass() external pure {
         assertEq(true, true);
     }
 }
