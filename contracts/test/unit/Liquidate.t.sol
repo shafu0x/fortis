b49da0e343a315eb50c30f61585060e357bad40f
shafu <shafu0x@gmail.com>
2024-12-25 21:49:26 +0100

    liquidate all

--- contracts/test/unit/Liquidate.t.sol
+++ contracts/test/unit/Liquidate.t.sol
@@ -15,6 +15,21 @@ contract Liquidate_Test is Base_Test {
         _setAssetPrice(1_200e8)
     {
         manager.liquidate(alice, 200e18, address(this));
+
+        assertEq(manager.minted(alice), 1000e18 - 200e18);
+    }
+
+    function test_liquidate_all() public 
+        _giveAssets   (alice, 100e18) 
+        _setAssetPrice(4_000e8)
+        _startPrank   (alice)
+        _deposit      (1e18, alice)
+        _mintFUSD     (1000e18, alice)
+        _setAssetPrice(1_200e8)
+    {
+        manager.liquidate(alice, 1000e18, address(this));
+
+        assertEq(manager.minted(alice), 0);
     }
 
     function test_liquidate_fail_notUndercollaterized() public 
