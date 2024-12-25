f254b4290fa8414aa8b751385d6ad828001fa006
shafu <shafu0x@gmail.com>
2024-12-25 17:26:00 +0100

    withdraw

--- contracts/test/unit/Withdraw.t.sol
+++ contracts/test/unit/Withdraw.t.sol
@@ -7,12 +7,26 @@ import {Base_Test} from "./Base.t.sol";
 
 contract Withdraw_Test is Base_Test {
     function test_withdraw() public 
-        _giveAssets   (alice, 100e18) 
-        _startPrank   (alice)
-        _depositTo    (100e18, alice, alice)
+        _giveAssets(alice, 100e18) 
+        _startPrank(alice)
+        _depositTo (100e18, alice, alice)
     {
         manager.withdraw(50e18, alice, alice);
 
+        assertEq(manager.deposited(alice), 50e18);
+    }
+
+    function test_withdraw_afterMint() public 
+        _giveAssets(alice, 100e18) 
+        _startPrank(alice)
+        _depositTo (100e18, alice, alice)
+        _mintFUSD  (1e18, alice, alice)
+    {
+        uint crBefore = manager.collatRatio(alice);
+        manager.withdraw(50e18, alice, alice);
+        uint crAfter  = manager.collatRatio(alice);
+
+        assertTrue(crBefore > crAfter);
         assertEq(manager.deposited(alice), 50e18);
     }
 }
