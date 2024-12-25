3bbb9195fc9578cdfe00ed876756e07ee78edc8b
shafu <shafu0x@gmail.com>
2024-12-25 19:56:36 +0100

    test liquidate

--- contracts/test/unit/Liquidate.t.sol
+++ contracts/test/unit/Liquidate.t.sol
@@ -6,4 +6,14 @@ import "forge-std/src/Test.sol";
 import {Base_Test} from "./Base.t.sol";
 
 contract Liquidate_Test is Base_Test {
+    function test_liquidate() public 
+        _giveAssets   (alice, 100e18) 
+        _setAssetPrice(4_000e8)
+        _startPrank   (alice)
+        _depositTo    (1e18, alice, alice)
+        _mintFUSD     (1000e18, alice, alice)
+        _setAssetPrice(1_200e8)
+    {
+        manager.liquidate(alice, 200e18, address(this));
+    }
 }
