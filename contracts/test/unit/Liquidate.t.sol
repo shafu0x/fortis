5c2ea6b81c8d569386ede6285e9e2f0e09b38b83
shafu <shafu0x@gmail.com>
2024-12-25 20:11:18 +0100

    yield test

--- contracts/test/unit/Liquidate.t.sol
+++ contracts/test/unit/Liquidate.t.sol
@@ -28,4 +28,16 @@ contract Liquidate_Test is Base_Test {
         vm.expectRevert("NOT_UNDERCOLLATERALIZED");
         manager.liquidate(alice, 200e18, address(this));
     }
+
+    function test_liquidate_fail_noDebtToRepay() public 
+        _giveAssets   (alice, 100e18) 
+        _setAssetPrice(4_000e8)
+        _startPrank   (alice)
+        _deposit      (1e18, alice)
+        _mintFUSD     (1000e18, alice)
+        _setAssetPrice(1_200e8)
+    {
+        vm.expectRevert("NO_DEBT_TO_REPAY");
+        manager.liquidate(alice, 0, address(this));
+    }
 }
